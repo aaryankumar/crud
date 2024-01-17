d21e137b76be88776c66d33bb1caa7a7f38dc67a
aaryan kumar <aaryankumarhmhs@gmail.com>
2024-01-18 00:16:24 +0530

    publish branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogTitle,
+  MatDialogContent,
+} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'crud';
+
+  constructor(private dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '50%',
+    });
+  }
 }
